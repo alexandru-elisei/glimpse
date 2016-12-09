2382290f5efc2ed442856840da411158f6fb393a
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-12-09 18:59:07 +0200

    Add support for multiple email accounts

--- glimpse.lua
+++ glimpse.lua
@@ -6,31 +6,65 @@
 local wibox = require("wibox")
 local os = os
 local timer = timer
+local table = table
 
 local glimpse = {mt = {}}
-
 local homedir = os.getenv("HOME")
 
 local defaults = {
     poll_interval = 300,
-    retry_interval = 5,
+    retry_interval = 10,
     tmpdir = homedir,
-    pydir = '/usr/share/awesome/lib'
+    pydir = '/usr/share/awesome/lib/glimpse/'
 }
 
+function glimpse:fetch_mail()
+    -- Setting all acounts' state to fetching mail.
+    for key, account in pairs(self.accounts) do
+        self.accounts[key].state = 'fetching'
+        os.execute('touch '..account.tmpfile..' && chmod 0600 '..account.tmpfile..' && PYTHONIOENCODING=utf8 '..self.pydir..'/glimpse.py '..account.conf..' > '..account.tmpfile..' &')
+    end
+end
+
+function glimpse:get_widget_text()
+    local widget_text = ''
+
+    for _, account in pairs(self.accounts) do
+        widget_text = widget_text..account.shortname..': '
+        if account.state == "fetching" then
+            widget_text = widget_text..'? '
+        elseif account.state == 'error' then
+            widget_text = widget_text..'ERR '
+        else
+            widget_text = widget_text..account.count..' '
+        end
+    end
+
+    return widget_text
+end
+
 function glimpse:start()
-    self.prev_count = 0
-    self.count = 0
 
-    self.widget:set_text(self.shortname .. ': ? ')
-    self.content = '\nConnecting with email server...'
+    for key, account in pairs(self.accounts) do
+        self.accounts[key].prev_count = 0
+        self.accounts[key].count = 0
+        self.accounts[key].content = '\n['..account.account..']\nFetching mail...\n'
+    end
 
-    os.execute(self.pydir..'/glimpse.py '..self.conf..' > '..self.tmpfile..' &')
+    self:fetch_mail()
+    self.widget:set_text(self:get_widget_text())
+    self.fetch_all = true
 
     self.timer = timer({timeout = retry_timemout})
     -- Using closures to preserve the object.
-    self.timer:connect_signal("timeout", function() glimpse.check_mail(self) end)
+    self.timer:connect_signal('timeout', function() glimpse.check_mail(self) end)
     self.timer:start()
+end
+
+function glimpse:stop()
+    self.timer:stop()
+    self.timer.timeout = 0
+    naughty.notify({title = 'Glimpse', text = '\nGlimpse stopped.'})
 end
 
 function glimpse:reset_timer(timeout)
@@ -39,50 +73,96 @@ function glimpse:reset_timer(timeout)
     self.timer:start()
 end
 
-function glimpse:check_mail()
-    local f = io.open(self.tmpfile)
-    if f == nil then
-        self:reset_timer(self.retry_interval)
-        return
-    end
+function glimpse:notify(all)
+    local notification_text = ''
 
-    self.count = f:read('*line')
-    if self.count == nil then
-        f:close()
-        self:reset_timer(self.retry_interval)
-        return
-    end
-
-    if string.match(self.count, '^[0-9]+$') then
-        self.count = tonumber(self.count)
-
-        -- Only preview new emails.
-        if self.count > self.prev_count then
-            self.content = '\n['..self.account..']\n'..f:read('*all')
-            naughty.notify({title = 'Email', text = self.content, timeout = 10})
+    for _, account in pairs(self.accounts) do
+        if all then
+            notification_text = notification_text..account.content
+            account.state = 'seen'
+        elseif account.state == 'new' then
+            notification_text = notification_text..account.content
+            account.state = 'seen'
+        elseif account.state == 'error' then
+            notification_text = notification_text..account.content
         end
-
-        if self.count ~= self.prev_count then
-            self.prev_count = self.count
-        end
-    else
-        -- Preview errors.
-        self.content = '\n['..self.account..']\n'..f:read('*all')
-        naughty.notify({title = 'Email', text = self.content, timeout = 10})
     end
 
-    f:close()
-
-    self.widget:set_text(self.shortname..': ' .. tostring(self.count))
-    os.execute(self.pydir..'/glimpse.py '..self.conf..' > '..self.tmpfile..' &')
-
-    self.timer:stop()
-    self.timer.timeout = self.poll_interval
-    self.timer:start()
+    if notification_text:len() > 0 then
+        naughty.notify({title = 'Glimpse', text = notification_text, timeout = 10})
+    end
 end
 
-local function test()
-    check_mail()
+function glimpse:check_mail()
+
+    if self.fetch_all then
+        self.fetch_all = false
+        self:fetch_mail()
+        self:reset_timer(self.retry_interval)
+        return
+    end
+
+    local fetch_timeout = false
+
+    for key, account in pairs(self.accounts) do
+        -- Only check accounts who are fetching the emails.
+        if account.state == 'fetching' then
+            local account_timeout = false
+            local f = io.open(account.tmpfile)
+
+            if f == nil then
+                account_timeout = true
+                fetch_timeout = true
+            end
+
+            local count
+
+            if not account_timeout then
+                count = f:read('*line')
+                if count == nil then
+                    f:close()
+                    account_timeout = true
+                    fetch_timeout = true
+                end
+            end
+
+            if not account_timeout then
+                if string.match(count, '^[0-9]+$') then
+                    count = tonumber(count)
+                    self.accounts[key].count = count
+
+                    -- Only preview new emails.
+                    if count > account.prev_count then
+                        self.accounts[key].content = '\n['..account.account..']\n'..f:read('*all')
+                    end
+
+                    if account.prev_count ~= count then
+                        self.accounts[key].prev_count = count
+                        self.accounts[key].state = 'new'
+                    else
+                        self.accounts[key].state = 'seen'
+                    end
+                else
+                    self.accounts[key].content = '\n['..account.account..']\n'..f:read('*all')
+                    self.accounts[key].state = 'error'
+                end
+
+                f:close()
+            end
+        end
+    end
+
+    self:notify()
+    self.widget:set_text(self:get_widget_text())
+
+    if fetch_timeout then
+        -- Retry reading the account temporary files.
+        self:reset_timer(self.retry_interval)
+    else
+        -- Queue polling all accounts.
+        self.fetch_all = true
+        self:reset_timer(self.poll_interval)
+    end
 end
 
 local function new(args)
@@ -95,24 +175,33 @@ local function new(args)
     pydir = pydir:gsub("^~", homedir)
     self.pydir = pydir
 
-    if args.account == nil then
-        return print('E: glimpse: error creating object: no accounts specified')
-    end
-    self.account = args.account
-
-    if args.shortname == nil then
-        return print('E: glimpse: error creating object: no account shortname specified')
-    end
-    self.shortname = args.shortname
-
-    if args.conf == nil then
-        return print('E: glimpse: error creating object: no configuration file specified')
-    end
-    self.conf = args.conf
-
     local tmpdir = args.tmpdir or defaults.tmpdir
     tmpdir = tmpdir:gsub("^~", homedir)
-    self.tmpfile = tmpdir..'/.'..self.shortname..'.out'
+
+    if args.accounts == nil then
+        return print('E: glimpse: error creating object: no accounts specified')
+    end
+    self.accounts = {}
+
+    for _, account in pairs(args.accounts) do
+        local new_account = {}
+        if account.account == nil then
+            return print('E: glimpse: error creating object: no account specified')
+        end
+        new_account.account = account.account
+
+        if account.shortname == nil then
+            return print('E: glimpse: error creating object: no shortname specified')
+        end
+        new_account.shortname = account.shortname
+
+        if account.conf == nil then
+            return print('E: glimpse: error creating object: no configuration file specified')
+        end
+        new_account.conf = account.conf
+        new_account.tmpfile = tmpdir..'/.glimpse_'..new_account.shortname..'.out'
+        table.insert(self.accounts, new_account)
+    end
 
     self.widget = wibox.widget.textbox()
 
