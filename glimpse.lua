e7d2aa1d0d58149cdb8f714cdb4c88563bc3d731
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-12-09 18:59:38 +0200

    Version 0.2.0

--- glimpse.lua
+++ glimpse.lua
@@ -1,6 +1,6 @@
 -- @author Alexandru Elisei &lt;alexandru.elisei@gmail.com&gt;
 -- @copyright 2016 Alexandru Elisei
--- @release 0.1.0
+-- @release 0.2.0
 
 -- Module environment
 local wibox = require("wibox")
